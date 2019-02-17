2715937e2a2b3e342995d683f0f1737520ee8b1a
weiwenchang <3081483560@qq.com>
2019-02-17 20:21:37 +0800

    jkg

---
+++ git/src/gitProject/Test1.java
@@ -0,0 +1,9 @@
+package gitProject;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("547");
+        System.out.println("547");
+        System.out.println("547");
+    }
+}
